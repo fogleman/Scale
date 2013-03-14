058038710d9aeba2ce18472456edd9b4a4a47121
Michael Fogleman <fogleman@gmail.com>
2013-03-13 22:45:34 -0400

    added todo

--- Scale/AppDelegate.h
+++ Scale/AppDelegate.h
@@ -5,9 +5,13 @@
 //  Created by Michael Fogleman on 2/24/13.
 //  Copyright (c) 2013 Michael Fogleman. All rights reserved.
 //
-// Icons:
-// http://www.iconfinder.com/search/?q=iconset%3Asuper-mono-sticker
-// http://www.doublejdesign.co.uk/
+// ICONS
+// - http://www.iconfinder.com/search/?q=iconset%3Asuper-mono-sticker
+// - http://www.doublejdesign.co.uk/
+//
+// TODO
+// - progress indicator?
+//
 
 #import <Cocoa/Cocoa.h>
 
