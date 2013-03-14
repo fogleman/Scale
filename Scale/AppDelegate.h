03872f95f0419e6ffadad8295d6db0491ab45af7
Michael Fogleman <fogleman@gmail.com>
2013-03-13 22:02:21 -0400

    picker icon

--- Scale/AppDelegate.h
+++ Scale/AppDelegate.h
@@ -5,6 +5,9 @@
 //  Created by Michael Fogleman on 2/24/13.
 //  Copyright (c) 2013 Michael Fogleman. All rights reserved.
 //
+// Icons:
+// http://www.iconfinder.com/search/?q=iconset%3Asuper-mono-sticker
+// http://www.doublejdesign.co.uk/
 
 #import <Cocoa/Cocoa.h>
 
